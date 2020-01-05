7071d92279cd42d3d7092ce19513668c8d22d152
Brahian <teslasapbe@gmail.com>
2020-01-05 19:37:38 -0300

    logger added

---
+++ logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log *zap.Logger
+)
+
+func init() {
+	logConfig := zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		//Saves the log in the defined file and prints in the console
+		OutputPaths: []string{"./logs/application.json","stdout"},
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	var err error
+	if log, err = logConfig.Build(); err != nil{
+		panic(err)
+	}
+}
+
+func GetLogger() *zap.Logger{
+	return log
+}
+
+func Info(msg string, tags ...zap.Field){
+	log.Info(msg, tags...)
+	log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field)  {
+
+	tags = append(tags, zap.NamedError("error", err))
+	log.Error(msg, tags...)
+	log.Sync()
+
+}
